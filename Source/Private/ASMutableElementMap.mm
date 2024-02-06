5207a54864c3ee95772174bfb877e488d52261c9
Francesco Cosentino <francesco@messagenius.com>
2024-02-06 12:14:19 +0100

    Revert "Update ASMutableElementMap.mm"
    This reverts commit 5d937867aa3588b5a8f826f4121908123098e8bd.

--- Source/Private/ASMutableElementMap.mm
+++ Source/Private/ASMutableElementMap.mm
@@ -55,13 +55,7 @@ typedef NSMutableDictionary<NSString *, NSMutableDictionary<NSIndexPath *, ASCol
 
 - (void)removeSectionsAtIndexes:(NSIndexSet *)indexes
 {
-    [indexes enumerateIndexesWithOptions:NSEnumerationReverse usingBlock:^(NSUInteger idx, BOOL * _Nonnull stop) {
-        if (idx < _sections.count) {
-            [_sections removeObjectAtIndex:idx];
-        }
-    }];
-    
-  //[_sections removeObjectsAtIndexes:indexes];
+  [_sections removeObjectsAtIndexes:indexes];
 }
 
 - (void)removeSupplementaryElementsAtIndexPaths:(NSArray<NSIndexPath *> *)indexPaths kind:(NSString *)kind
@@ -77,13 +71,7 @@ typedef NSMutableDictionary<NSString *, NSMutableDictionary<NSIndexPath *, ASCol
 
 - (void)removeSectionsOfItems:(NSIndexSet *)itemSections
 {
-    [itemSections enumerateIndexesWithOptions:NSEnumerationReverse usingBlock:^(NSUInteger idx, BOOL * _Nonnull stop) {
-        if (idx < _sectionsOfItems.count) {
-            [_sectionsOfItems removeObjectAtIndex:idx];
-        }
-    }];
-
-  //[_sectionsOfItems removeObjectsAtIndexes:itemSections];
+  [_sectionsOfItems removeObjectsAtIndexes:itemSections];
 }
 
 - (void)insertEmptySectionsOfItemsAtIndexes:(NSIndexSet *)sections
